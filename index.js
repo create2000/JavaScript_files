05fba27caee6a13b1345effc82e8ec6b07fb5ca4
Anthony Onah <onah582@gmail.com>
2023-08-11 13:16:03 +0100

    Today's class on JS Objects

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
-const myObject = {
-    firstname: 'John',
-    lastname: 'Smith',
-    hobbies: ['sleeping', 'eating', 'coding'],
-    beverages: {
-        morning: "coffee",
-        afternoon: "Iced Tea"
-    },
-    action: function () {
-        return `Time for ${this.beverages.afternoon} and 
-        ${this.beverages.morning}`;
-    }
-};
-
-    myObject.middleName = 'Moses';
-
-console.log(myObject);
-
-
-
-
-    myObject.firstname = 'Jane';
-    console.log(myObject);
-//     myObject.fullname = myObject.firstname + ' ' + myObject.lastname
-
-// console.log(myObject.action());
-
-const vehicle = {
-    wheels: 4,
-    engine: function ()  {
-        return 'Vrooommm'
-    }
-};
-
-console.log(vehicle.engine());
-// const cars = Object.create(vehicle);
-// cars.doors = 4;
-// cars.engine = function () {
-//     return 'whoooooshh'
+// const myObject = {
+//     firstname: 'John',
+//     lastname: 'Smith',
+//     hobbies: ['sleeping', 'eating', 'coding'],
+//     beverages: {
+//         morning: "coffee",
+//         afternoon: "Iced Tea"
+//     },
+//     action: function () {
+//         return `Time for ${this.beverages.afternoon} and 
+//         ${this.beverages.morning}`;
+//     }
 // };
 
-// console.log(cars.engine());
+//     myObject.middleName = 'Moses';
+
+// console.log(myObject);
 
 
-const John = {
-    name: 'John',
-    weight: 50,
-    hobbies : ['sleeping', 'eating', 'coding'],
-    colour: 'melanin',
-    age: 30,
-    job: true,
-    fullname: function () {
-        return `His name is ${this.name} and his weight is ${this.weight}`
+
+
+//     myObject.firstname = 'Jane';
+//     console.log(myObject);
+// //     myObject.fullname = myObject.firstname + ' ' + myObject.lastname
+
+// // console.log(myObject.action());
+
+// const vehicle = {
+//     wheels: 4,
+//     engine: function ()  {
+//         return 'Vrooommm'
+//     }
+// };
+
+// console.log(vehicle.engine());
+// // const cars = Object.create(vehicle);
+// // cars.doors = 4;
+// // cars.engine = function () {
+// //     return 'whoooooshh'
+// // };
+
+// // console.log(cars.engine());
+
+
+// const John = {
+//     name: 'John',
+//     weight: 50,
+//     hobbies : ['sleeping', 'eating', 'coding'],
+//     colour: 'melanin',
+//     age: 30,
+//     job: true,
+//     fullname: function () {
+//         return `His name is ${this.name} and his weight is ${this.weight}`
         
         
-    }
-}
-console.log(John.fullname());
-
-
-// function number(a, b) {
-//     return a + b;
-
+//     }
 // }
+// console.log(John.fullname());
 
-// console.log(5 * 10);
+
+// // function number(a, b) {
+// //     return a + b;
+
+// // }
+
+// // console.log(5 * 10);
+
+let name = ' ';
+
+
+
+console.log(name);
